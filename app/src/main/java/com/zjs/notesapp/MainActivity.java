d7a82935f23f0202a64f41894a7bc0c1004e0c3d
zszillat <zachary.szillat@outlook.com>
2024-05-17 16:00:14 -0400

    recyclerview setup

--- app/src/main/java/com/zjs/notesapp/MainActivity.java
+++ app/src/main/java/com/zjs/notesapp/MainActivity.java
@@ -7,13 +7,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerViewFiles;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
 
     }
 }
