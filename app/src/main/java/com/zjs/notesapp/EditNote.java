48d0c06f6b6e6a3307ba37185badedd50730f8b6
zszillat <zachary.szillat@outlook.com>
2024-05-31 16:41:46 -0400

    setup layouts

---
+++ app/src/main/java/com/zjs/notesapp/EditNote.java
@@ -0,0 +1,18 @@
+package com.zjs.notesapp;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class EditNote extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_note);
+    }
+}
